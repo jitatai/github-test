6f24cf306e7e970f0874f5f3f122941ad1897d35
佳太 <846722168@qq.com>
2019-08-16 02:02:19 +0800

    将项目提交到本地仓库

---
+++ first-git/src/com/jt/demo/FirstClass.java
@@ -0,0 +1,11 @@
+package com.jt.demo;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("first main");
+        System.out.println("first main");
+        System.out.println("first main");
+        System.out.println("first main");
+        System.out.println("first main");
+    }
+}
